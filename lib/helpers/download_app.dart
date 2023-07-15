ad8257532de96146e722cdd3ff18882305acdfeb
QCarder <qcarder@pc-367.home>
2023-07-16 01:14:22 +0300

    continue implementing ios

--- lib/helpers/download_app.dart
+++ lib/helpers/download_app.dart
@@ -1,11 +1,12 @@
 import 'package:url_launcher/url_launcher.dart';
 
 downloadApp() async {
+  // TODO
   // Replace with your Flutter app's deep link URL scheme
-  const String appDeepLink = "myflutterapp://";
+  const String appDeepLink = "qcarder://";
 
   const String appStoreURL =
-      "https://play.google.com/store/apps/details?id=com.example.myflutterapp";
+      "https://play.google.com/store/apps/details?id=com.qcarder.qcarder";
 
   bool isAppInstalled = await canLaunchUrl(Uri(path: appDeepLink));
 
